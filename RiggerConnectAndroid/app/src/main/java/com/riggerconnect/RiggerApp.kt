1251302fcc4ec35f8b6226e83b5d448d47440bbf
Tiation <tiatheone@protonmail.com>
2025-07-23 19:38:24 +0800

    Remove deprecated RiggerConnect Android files
    - Remove outdated Android build configuration files
    - Clean up deprecated source code and manifest files
    - Remove unused service layer implementations
    - Clean up resource files and build scripts
    
    This commit removes deprecated Android application files as part of the
    tiation-ecosystem restructuring and migration to dedicated repositories.

--- RiggerConnectAndroid/app/src/main/java/com/riggerconnect/RiggerApp.kt
+++
@@ -1,7 +0,0 @@
-package com.riggerconnect
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class RiggerApp : Application()
--- RiggerConnectAndroid/app/src/main/java/com/tiation/riggerconnect/RiggerConnectApp.kt
+++
@@ -1,7 +0,0 @@
-package com.tiation.riggerconnect
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class RiggerConnectApp : Application()
